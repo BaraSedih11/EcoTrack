dc149c051533672bf20d98e7eeb2d39502fde2a9
BaraAlSedih <98843912+BaraSedih11@users.noreply.github.com>
2024-01-23 18:39:02 +0200

    Create ecotrack_contributions.sql

---
+++ database/ecotrack_contributions.sql
@@ -0,0 +1,60 @@
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: ecotrack
+-- ------------------------------------------------------
+-- Server version	8.2.0
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `contributions`
+--
+
+DROP TABLE IF EXISTS `contributions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `contributions` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `userId` int NOT NULL,
+  `contributionType` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `location` varchar(255) NOT NULL,
+  `date` date NOT NULL,
+  `quantity` int NOT NULL,
+  `units` varchar(255) NOT NULL,
+  `additionalDetails` text,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `contributions`
+--
+
+LOCK TABLES `contributions` WRITE;
+/*!40000 ALTER TABLE `contributions` DISABLE KEYS */;
+INSERT INTO `contributions` VALUES (1,1,'Planting Trees','Planted 10 trees in the community park.','Community Park, City X','2023-11-01',10,'trees','Used eco-friendly planting materials.','2023-12-24 21:25:59','2023-12-24 21:25:59'),(2,1,'Planting Trees','Planted 10 trees in the community park.','Community Park, City X','2023-11-01',10,'trees','Used eco-friendly planting materials.','2023-12-24 21:27:35','2023-12-24 21:27:35'),(3,1,'Moving trash','6 hours moving trash in the community park.','Community Park, City Y','2023-10-07',6,'hours','Used eco-friendly cleaning tools.','2023-12-28 12:01:11','2023-12-28 12:01:11'),(4,9,'Moving trash','6 hours moving trash in the community park.','Community Park, City Y','2023-10-07',6,'hours','Used eco-friendly cleaning tools.','2023-12-28 12:02:29','2023-12-28 12:02:29');
+/*!40000 ALTER TABLE `contributions` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-12-28 19:33:59
